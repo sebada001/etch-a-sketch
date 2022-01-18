6616e84cb9c52a34371f03e86dd9e29b5afed46e
Sebastian Schumacher <sebastianschumacher10@gmail.com>
2022-01-18 15:36:56 -0400

    layout v1

--- js.js
+++ js.js
@@ -1,31 +1,64 @@
-var cellNumber = 36; //take grid column value as you want
-
 
 var container = document.querySelector(".container");
 
-for (var i = 0; i < cellNumber; i++){
-    var cell = document.createElement("div");
-    // cell.style.border = "1px solid red";
-    cell.style.backgroundColor = "green";
-    cell.style.width = "1fr";
-    cell.style.height = "1fr";
-    // cell.style.aspectRatio = "1/1";
-    container.appendChild(cell); 
-    cell.addEventListener("mouseover", function(event){
-        event.target.style.backgroundColor = "blue";
-    })
-    cell.addEventListener("mouseleave", function(eventTwo){
-        eventTwo.target.style.backgroundColor = "green";
-    })
-    
+var mouseDown = 0;
+
+document.body.onmousedown = function() { 
+  ++mouseDown;
+}
+document.body.onmouseup = function() {
+  --mouseDown;
 }
 
-container[container.length-1].addEventListener("click", function () {
-    alert("test");
-}, false);
+function makeRows(rows, cols){
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (var i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        cell.addEventListener("mouseleave", function(event){
+            if (mouseDown){
+                event.target.style.backgroundColor = "var(--light-color-1)";
+            }
+            else{
+                return
+            }
+        }) 
+        cell.addEventListener("mouseenter", function(eventTwo){
+            if (mouseDown){
+                eventTwo.target.style.backgroundColor = "var(--light-color-1)";
+            }
+            else{
+                return
+            }
+        })
+        cell.addEventListener("click", function(eventThree){
+            eventThree.target.style.backgroundColor = "var(--light-color-1)";
+        })
+        container.appendChild(cell).className = "grid-item";
+    }
+}
 
 
+makeRows(16, 16);
 
+function gridMaker(){
+    var gridValue = document.getElementById("inputNumber").value;
+    if (gridValue >= 100){
+        gridValue = 100;
+    }
+    removeChildren(container);
+    makeRows(gridValue, gridValue);
+}
+
+function removeChildren(parent){
+    while (parent.lastChild) {
+        parent.removeChild(parent.lastChild);
+    }
+};
+
+function checkCheckbox(){
+    
+}
 
 console.log(container)
 
